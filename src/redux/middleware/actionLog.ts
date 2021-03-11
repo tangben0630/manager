e07a2f25c809870b248a8530e2506f2908a7cb52
tangben <240193690@qq.com>
2021-03-11 09:21:09 +0800

    .

--- src/redux/middleware/actionLog.ts
+++ src/redux/middleware/actionLog.ts
@@ -1,7 +1,7 @@
 import { Middleware } from 'redux'
 export const actionLog: Middleware = (store) => (next) => (action) => {
-  console.log('state 当前', store.getState().ListReducer.lan);
-  console.log('fire action', action);
+  // console.log('state 当前', store.getState().ListReducer.lan);
+  // console.log('fire action', action);
   next(action)
-  console.log('state更改后的', store.getState().ListReducer.lan);
+  // console.log('state更改后的', store.getState().ListReducer.lan);
 }
